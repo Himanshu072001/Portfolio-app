2c9803197837758a281e43e9b8f7e7bfa9f3e2c6
Himanshu Kesharwani <himanshu.kesharwani.02@gmail.com>
2024-07-29 23:09:00 +0530

    [ADD]: Initial project setup.

---
+++ Portfolio-App/Portfolio-App/Portfolio_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Portfolio_AppApp.swift
+//  Portfolio-App
+//
+//  Created by Himanshu Kesharwani on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Portfolio_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
